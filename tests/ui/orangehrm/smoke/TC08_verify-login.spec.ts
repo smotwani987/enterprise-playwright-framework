86615fac7ed88fb5b547c424657d734741e869e7
smotwani987 <82025645+smotwani987@users.noreply.github.com>
2026-07-13 17:15:13 +0530

    Update TC08_verify-login.spec.ts

--- tests/ui/orangehrm/smoke/TC08_verify-login.spec.ts
+++ tests/ui/orangehrm/smoke/TC08_verify-login.spec.ts
@@ -1,7 +1,7 @@
 import {test,expect} from '../../../../fixtures/baseFixture';
 import { ENV } from '../../../../config/env';
 
-test.describe('@smoke OrangeHRM Login Test', () => {
+test.describe('@smoke @aws-smoke OrangeHRM Login Test', () => {
 
   test('TC08 - Verify Login', async ({poManager}) => {
     await poManager.LoginPage.navigate();
